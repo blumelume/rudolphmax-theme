ac10a63f1df136b0d4978321d3d77ffc901be8eb
Maximilian Rudolph <rudolph-max@gmx.net>
2023-02-18 16:30:20 +0100

    ditching text-wrap detection

--- js/main_nav.js
+++ js/main_nav.js
@@ -1,39 +1,12 @@
 window.addEventListener("load",  function () {
-  let mainNav = document.querySelector('nav#main');
-  let mainNavList = mainNav.querySelector('.nav-list');
-
-  // Adding observers to all nav-items
-  Array.from( mainNavList.childNodes ).forEach( (el) => {
-    if (typeof el.classList != 'undefined') {
-
-      //handles style changes on banner to check wrapping
-      const observer = new MutationObserver(mutations =>
-        mutations.forEach(mutationRecord => mobilizeMainNav())
-      );
-      observer.observe(el, { attributes : true, attributeFilter : ['style'] });
-    }
-  });
-
+  const mainNav = document.querySelector('nav#main');
 
   function mobilizeMainNav() {
-    Array.from( mainNavList.childNodes ).forEach( (el) => {
-      if (typeof el.classList != 'undefined') {
-        if (el.classList.contains('nav-item') && !el.classList.contains('current')) {
-
-          const text = el.querySelector('.text');
-          const lineHeight = parseInt(getComputedStyle(text).lineHeight, 10);
-          const amountOfLinesTilAdjust = 2;
-
-          if (text.offsetHeight >= (lineHeight * amountOfLinesTilAdjust)) {
-            mainNav.classList.add('mobile', 'collapsed');
-          } else {
-            mainNav.classList.remove('mobile');
-          }
-
-        }
-
-      }
-    });
+    if ( mainNav.offsetWidth >= window.innerWidth ) {
+      mainNav.classList.add('mobile', 'collapsed');
+    } else {
+      mainNav.classList.remove('mobile', 'collapsed');
+    }
   }
 
   window.addEventListener("resize", mobilizeMainNav);
