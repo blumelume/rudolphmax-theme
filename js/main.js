148fc373a0e72be041b7df61dbf998fd930ba09d
Maximilian Rudolph <rudolph-max@gmx.net>
2023-02-18 23:39:25 +0100

    feat: stages with classes and more elaborate styling

---
+++ js/main.js
@@ -0,0 +1,21 @@
+window.addEventListener('load', function () {
+
+  Array.from( document.getElementsByClassName('jump-stage') ).forEach( (el) => {
+    el.setAttribute( 'aria-label', 'jump from stage to content' )
+    
+    el.addEventListener('click', (e) => {
+      let targetStage = e.target.parentNode;
+
+      if (targetStage) {
+        let scrollTarget = targetStage.getBoundingClientRect().bottom;
+
+        window.scroll({
+          behavior: 'smooth',
+          left: 0,
+          top: scrollTarget - 50
+        });
+      }
+    })
+  })
+
+})
