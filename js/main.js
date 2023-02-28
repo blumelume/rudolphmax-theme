4a8bc6db0e03c7dac1bf9d417d3a7b80d5542575
Maximilian Rudolph <rudolph-max@gmx.net>
2023-02-28 21:57:47 +0100

    fix: addressing linter

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
-import * as mainNav from "./modules/main-nav.js"
-import * as components from "./modules/components.js";
+import * as mainNav from './modules/main-nav.js'
+import * as components from './modules/components.js';
 
-mainNav.initializeMainNav();
+mainNav.initializeMainNav()
 
 window.addEventListener('load', function () {
-
   Array.from( 
     document.getElementsByClassName('jump-stage')
-  ).forEach( components.initializeJumpStageButton )
-  
+  ).forEach(components.initializeJumpStageButton)
+
   components.insertYear('cr-year')
 })
--- js/modules/components.js
+++ js/modules/components.js
@@ -3,9 +3,9 @@
  * @param {string} crYearSelector The ID selector for the element the should be inserted into.
  */
 export function insertYear (crYearSelector) {
-  let crYear = document.getElementById(crYearSelector);
+  const crYear = document.getElementById(crYearSelector)
   if (crYear) {
-    crYear.innerHTML = new Date().getFullYear();
+    crYear.innerHTML = new Date().getFullYear()
   }
 }
 
@@ -15,11 +15,11 @@ export function insertYear (crYearSelector) {
  * @param {HTMLElement} button A DOM Element reference to a JumpStage button.
  */
 export function initializeJumpStageButton (button) {
-  button.setAttribute( 'aria-label', 'jump from stage to content' )
+  button.setAttribute('aria-label', 'jump from stage to content')
   button.addEventListener('click', (e) => {
-    const targetStage = e.target.parentNode;
+    const targetStage = e.target.parentNode
     if (targetStage) {
-      let scrollTarget = targetStage.getBoundingClientRect().bottom;
+      const scrollTarget = targetStage.getBoundingClientRect().bottom
 
       window.scroll({
         behavior: 'smooth',
